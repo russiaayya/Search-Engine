f7b15d65416ac0759c63e3e1f6dbd0124dbbb553
Pradeepa Gopinath <pradeepagopinath19@gmail.com>
2018-04-22 15:08:26 -0400

    Add files via upload

---
+++ corpus.py
@@ -0,0 +1,71 @@
+import string
+from urllib.request import urlopen
+import bs4 as bsnew
+from nltk import word_tokenize
+from string import punctuation
+
+
+def hasNumbersNoAlpha(inputString):# function to make sure it contains only the digits and punctuations and no letters
+    if any(c.isalpha() for c in inputString):
+        return False
+    if any(char.isdigit() for char in inputString):
+        return True
+
+
+
+def generateCorpus(dir):
+    filelist = os.listdir(os.path.abspath(dir))
+    print(filelist)
+    validLinks = file.readlines()
+    file.close()
+    validTokens = []
+    punctuation = string.punctuation.replace("-", "")  # retaining hyphen- question
+    count = 0
+    for url in validLinks:
+        print(url)
+        if len(url) <= 1:
+            continue
+        urlindex = url.index('wiki/')
+        specificUrl = url[urlindex + 5:]
+        specificUrl = specificUrl.strip("\n")
+        filename = r"bfsFiles/" + specificUrl + ".html"
+        file_contents = open(filename, "r", encoding='utf-8').read()
+        bs = bsnew.BeautifulSoup(file_contents, "html.parser")
+        titles = specificUrl.split("_")
+        content = bs.find("div", {"id": "mw-content-text"})
+        for a in content.findAll("table"):
+            a.decompose()
+        for a in content.findAll("img"):
+            a.decompose()
+        for a in content.findAll("pre"):
+            a.decompose()
+        for a in content.findAll("code"):
+            a.decompose()
+        raw = content.get_text()
+        contents = str(raw).split()
+        contents.extend(titles)
+        for t in contents:
+            if "http" in t:#ignoring links
+                continue
+            if hasNumbersNoAlpha(t):
+                validTokens.append(t)
+                continue
+            if choice == "1":
+                t = t.lower()
+                for char in t:
+                    if char in punctuation:
+                        t = t.replace(char, "")
+            if len(t) >= 1:
+                validTokens.append(t)
+        urlindex = url.index('wiki/')
+        specificUrl = url[urlindex + 5:]
+        specificUrl = specificUrl.strip("\n")
+        filename = r"corpus/" + specificUrl + ".txt"
+        newFile = open(filename, 'w', encoding='utf-8')
+        newFile.write(str(validTokens))
+        validTokens = []
+        newFile.close()
+
+
+if __name__ == "__main__":
+    generateCorpus("D:\IR_project\rawDocuments")
