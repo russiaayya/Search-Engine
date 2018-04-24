04c9fd8c24125350efddf40343e05755434648d4
russiaaiyappa <russia.ayya@gmail.com>
2018-04-24 11:57:46 -0400

    comments

--- Task 3/tokenizer_Stemming.py
+++ Task 3/tokenizer_Stemming.py
@@ -2,9 +2,10 @@ import os
 import string
 import bs4 as bsnew
 
-
+# function that generates tokens from the given stemmed corpus
 def generateTokens():
     inputFile = open(r'Input\cacm_stem.txt', 'r', encoding='utf8')
+    # files contains a list of all the stemmed content for each document in the corpus
     files = inputFile.read().split('#')
     docID = 0
     for file in files:
@@ -16,6 +17,7 @@ def generateTokens():
             outputFile = open(r'tokenized_Files_Stemmed\CACM-%04d.txt' % docID, 'w', encoding='utf8')
             contents = file.split()
             contents.pop(0)
+            # ignore digits that commonly appear in the end of the documents' contents
             lastIndex = 0
             if "am" in contents:
                 lastIndex = rindex(contents, "am")
@@ -28,7 +30,7 @@ def generateTokens():
             outputFile.write(str(validTokens))
     outputFile.close()
 
-
+# returns index of myvalue in mylist
 def rindex(mylist, myvalue):
     return len(mylist) - mylist[::-1].index(myvalue) - 1
 
