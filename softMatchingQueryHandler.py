9813fa4951ee3f149a0061706f57efa73e6d1897
Pradeep Gopinath <pradeepagopinath19@gmail.com>
2018-04-24 19:12:07 -0400

    changes

--- softMatchingQueryHandler.py
+++ softMatchingQueryHandler.py
@@ -1,10 +1,12 @@
 from difflib import get_close_matches
 
-def getSoftMatchingQueryTerm(word,patterns):
-    print(get_close_matches(word, patterns))
-
+def getSoftMatchingQueryTerm(word,corpus):
+    listOfCorrections=get_close_matches(word, corpus)
+    for w in listOfCorrections:
+        if len(w)==len(word) and w not in stop_Words:
+            return w
 if __name__ == "__main__":
-    file_contents = open("queries.txt", 'r', encoding='utf-8')
+    file_contents = open("SEG_queries.txt", 'r', encoding='utf-8')
     queries = eval(file_contents.read())
     file_contents.close()
     file_contents = open("common_words.txt", 'r', encoding='utf-8')
@@ -13,9 +15,18 @@ if __name__ == "__main__":
     file_contents = open("unigram_index.txt", 'r', encoding='utf-8')
     unigrams = eval(file_contents.read())
     file_contents.close()
-    pattern=[]
+    corpus=[]
     for term in unigrams:
-        pattern.append(term)
+        corpus.append(term)
     for qid in queries.keys():
         for word in queries[qid]:
-            correctWord=getSoftMatchingQueryTerm(word,pattern)
+            if word in stop_Words:
+                continue
+            print("Original word:",word)
+            correctWord = ""
+            if word not in corpus:
+                correctWord=getSoftMatchingQueryTerm(word,corpus)
+            if correctWord=="":
+                correctWord=word
+            print("Corrected word:",correctWord)
+            
