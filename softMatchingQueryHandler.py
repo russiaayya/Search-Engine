9fd0a94a343c1f050b05939f23b599ced8f5c919
Pradeep Gopinath <pradeepagopinath19@gmail.com>
2018-04-24 21:12:31 -0400

    next

--- softMatchingQueryHandler.py
+++ softMatchingQueryHandler.py
@@ -1,10 +1,22 @@
 from difflib import get_close_matches
 
-def getSoftMatchingQueryTerm(word,corpus):
+def getSoftMatchingQueryTerm(word,corpus,unigrams):
     listOfCorrections=get_close_matches(word, corpus)
+    bestMatch=""
+    maxFreq = 0
     for w in listOfCorrections:
-        if len(w)==len(word) and w not in stop_Words:
-            return w
+        freq=getFrequency(w,unigrams)
+        if freq>maxFreq and w not in stop_Words and abs(len(w)-len(word))<=1:
+            maxFreq=freq
+            bestMatch=w
+    return bestMatch
+def getFrequency(w,unigrams):
+    freq=0
+    listOfDocs=unigrams[w]
+    for doc in listOfDocs.keys():
+        freq+=listOfDocs[doc]
+    return freq
+
 if __name__ == "__main__":
     count=0
     file_contents = open("SEG_queries.txt", 'r', encoding='utf-8')
@@ -23,14 +35,14 @@ if __name__ == "__main__":
         for word in queries[qid]:
             if word in stop_Words:
                 continue
-            #print("Original word:",word)
+            print("Original word:",word)
             correctWord = ""
             if word not in corpus:
-                correctWord=getSoftMatchingQueryTerm(word,corpus)
+                correctWord=getSoftMatchingQueryTerm(word,corpus,unigrams)
             if correctWord=="" or correctWord is None:
                 correctWord=word
-            #print("Corrected word:",correctWord)
+            print("Corrected word:",correctWord)
             if correctWord not in corpus:
                 count+=1
-                #print("not corrected for ",correctWord)
+                print("not corrected for ",correctWord)
     print(count)
