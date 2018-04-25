0c11e7fd4ce81623adf2c2af9e9c355fc71897e9
Pradeep Gopinath <pradeepagopinath19@gmail.com>
2018-04-25 01:23:49 -0400

    task2

--- softMatchingQueryHandler.py
+++ softMatchingQueryHandler.py
@@ -1,15 +1,30 @@
 from difflib import get_close_matches
 from autocorrect import spell
-def getSoftMatchingQueryTerm(word,corpus,unigrams):
+def getSoftMatchingQueryTerm(index,word,query,corpus,unigrams,bigrams):
     listOfCorrections=get_close_matches(word, corpus)
     bestMatch=""
     maxFreq = 0
     for w in listOfCorrections:
+        corelation=getCorelation(index,w,query,bigrams)
+        if corelation>0 and w not in stop_Words and abs(len(w)-len(word))<=1:
+            return w
         freq=getFrequency(w,unigrams)
         if freq>maxFreq and w not in stop_Words and abs(len(w)-len(word))<=1:
             maxFreq=freq
             bestMatch=w
     return bestMatch
+
+def getCorelation(index,w,query,bigrams):
+    value = 0
+    if index>0:
+        bigram_word=query[index-1]+" "+w
+    else:
+        return 0
+    if bigram_word in bigrams:
+        listOfDocs=bigrams[bigram_word]
+        for doc in listOfDocs:
+            value+=listOfDocs[doc]
+    return value
 def getFrequency(w,unigrams):
     freq=0
     listOfDocs=unigrams[w]
@@ -29,20 +44,23 @@ if __name__ == "__main__":
     file_contents = open("unigram_index.txt", 'r', encoding='utf-8')
     unigrams = eval(file_contents.read())
     file_contents.close()
+    file_contents = open("bigram_index.txt", 'r', encoding='utf-8')
+    bigrams = eval(file_contents.read())
+    file_contents.close()
     corpus=[]
     for term in unigrams:
         corpus.append(term)
     for qid in queries.keys():
         #print("Erroneous Queries",queries[qid])
         updatedQList=[]
-        for word in queries[qid]:
+        for index,word in enumerate(queries[qid]):
             if word in stop_Words:
                 updatedQList.append(word)
                 continue
             #print("Original word:",word)
             correctWord = ""
             if word not in corpus:
-                correctWord=getSoftMatchingQueryTerm(word,corpus,unigrams)
+                correctWord=getSoftMatchingQueryTerm(index,word,queries[qid],corpus,unigrams,bigrams)
             else:
                 updatedQList.append(word)
             if correctWord=="" or correctWord is None:
@@ -52,7 +70,7 @@ if __name__ == "__main__":
             #print("Corrected word:",correctWord)
             if correctWord not in corpus:
                 count+=1
-                #print("not corrected for ",correctWord)
+                # print("not corrected for ",correctWord)
                 if spell(correctWord) in corpus:
                     count-=1
                     updatedQList.append(spell(correctWord))
